b73dc2cee7627660850509d2a24f66ad8482cdf8
Mikael <mikaelel902@gmail.com>
2025-05-12 21:39:59 +0700

    tambah ajax

---
+++ video21/ajax/mahasiswa.php
@@ -0,0 +1,43 @@
+<?php
+require '../functions.php';
+$keyword = $_GET["keyword"];
+
+$query = "SELECT * FROM mahasiswa 
+                    WHERE 
+                name LIKE '%$keyword%' OR
+                nrp LIKE '%$keyword%' OR
+                jurusan LIKE '%$keyword%' 
+                ";
+$mahasiswa = query($query);
+
+
+?>
+   <table border="1" callpadding="50" callspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <?php $i = 1;?>
+         <?php foreach($mahasiswa as $row): ?>
+        <tr>
+            <td><?= $i;?></td>
+            <td>
+                <a href="ubah.php?id=<?=  $row["id"];?>">ubah</a>  |
+                <a href="hapus.php?id=<?= $row["id"];?>" onclick="return confirm('Yakin Deck?');">hapus</a>
+            </td>
+            <td><img src="img/<?php echo $row["gambar"];?>" alt="" width="50"></td>
+            <td><?= $row["nrp"]; ?></td>
+            <td><?= $row["name"];?></td>
+            <td><?= $row["email"];?></td>
+            <td><?= $row["jurusan"];?>Informatika</td>
+        </tr>
+        <?php $i++?>
+         <?php endforeach; ?>
+
+    </table>
---
+++ video21/index.php
@@ -0,0 +1,79 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION["login"])){//jika tidak ada session login maka arahkan ke form login
+    header("Location: login.php");
+    exit;
+}
+
+//koneksi ke database
+require 'functions.php';
+$mahasiswa = query("SELECT *FROM mahasiswa");
+
+//tombol cari di klik
+if(isset($_POST["cari"])){
+    $mahasiswa = cari($_POST["keyword"]);
+}
+?>
+<html>
+    <head>
+        <title>Halaman Admin</title>
+    </head>
+    <body>
+
+    <a href="logout.php">Logout</a>
+        
+    <h1>Daftar mahasiswa</h1>
+
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
+
+    <form action="" method="post">
+        <input type="text" name = "keyword" size="30" autofocus=""
+         placeholder="Cari Id" autocomplete="off" id="keyword">
+        <button type="submit" name="cari" id="tombol-cari">Cari!</button>
+
+    </form>
+
+
+    <br>
+
+    <div id="container">
+    <table border="1" callpadding="50" callspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <?php $i = 1;?>
+         <?php foreach($mahasiswa as $row): ?>
+        <tr>
+            <td><?= $i;?></td>
+            <td>
+                <a href="ubah.php?id=<?=  $row["id"];?>">ubah</a>  |
+                <a href="hapus.php?id=<?= $row["id"];?>" onclick="return confirm('Yakin Deck?');">hapus</a>
+            </td>
+            <td><img src="img/<?php echo $row["gambar"];?>" alt="" width="50"></td>
+            <td><?= $row["nrp"]; ?></td>
+            <td><?= $row["name"];?></td>
+            <td><?= $row["email"];?></td>
+            <td><?= $row["jurusan"];?>Informatika</td>
+        </tr>
+        <?php $i++?>
+         <?php endforeach; ?>
+
+    </table>
+    </div>
+
+    <script src="js/script.js">
+
+    </script>
+
+    </body>
+</html>
